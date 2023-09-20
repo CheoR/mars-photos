91aa9171f442c0570d81e4c1a8fd83b8ae6085c8
CheoR <cheo.roman+github@gmail.com>
2023-09-20 11:41:54 -0500

    build: replace Retrofit ScalarsConverterFactory with kotlinx serialization

--- app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
+++ app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
@@ -1,19 +1,21 @@
 package com.example.marsphotos.network
 
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
 
 
 private const val BASE_URL = "https://android-kotlin-fun-mars-server.appspot.com"
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create()) // to convert to string
+    .addConverterFactory(Json.asConverterFactory("application/json".toMediaType())) // to convert to string
     .baseUrl(BASE_URL)
     .build()
 
 interface MarsApiService {
     @GET("photos") // get request to /photos endpoint
-    suspend fun getPhotos(): String
+    suspend fun getPhotos(): List<MarsPhoto>
 
 }
 
---
+++ app/src/main/java/com/example/marsphotos/network/MarsPhoto.kt
@@ -0,0 +1,15 @@
+package com.example.marsphotos.network
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class MarsPhoto(
+    // Note: each variable corresponds to key name in response JSON object.
+    val id: String,
+    // where img_src comes from response but want to use imgSrc
+    @SerialName(value = "img_src")
+    val imgSrc: String
+
+)
+
